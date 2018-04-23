fe2cc13feb60b8362e38af69d6468cb5ea441055
Durev <vincent.durewski@gmail.com>
2018-04-23 11:13:59 +0200

    Merge the 2 words lists to use all words

--- level3/word_list.rb
+++ level3/word_list.rb
@@ -4,17 +4,13 @@ require_relative 'key_pad_convertor'
 class WordList
 
   WORDS_LIST_URL = "http://norvig.com/ngrams/word.list"
-  WORDS_COUNT_URL = "http://norvig.com/ngrams/count_1w.txt"
+  MOST_USED_WORDS_LIST_URL = "http://norvig.com/ngrams/count_1w.txt"
 
-  def initialize(words_list_path = "level3/words.list", words_count_path = "level3/words_count.txt")
+  def initialize(words_list_path = "level3/words.list", most_used_words_list_path = "level3/words_count.txt")
     @words_list_path = words_list_path
-    @words_count_path = words_count_path
+    @most_used_words_list_path = most_used_words_list_path
     import_words_list unless File.exists?(@words_list_path)
-    import_words_count unless File.exists?(@words_count_path)
-  end
-
-  def length_filtered_list(n)
-    ordered_words_list.select{ |word| word.length == n }
+    import_most_used_words_sorted_list unless File.exists?(@most_used_words_list_path)
   end
 
   def possible_words(n, taps_sequence)
@@ -23,12 +19,22 @@ class WordList
 
   private
 
-    def words_list
-      File.open(@words_list_path).read.split("\n")
+    attr_reader :words_list_path, :most_used_words_list_path
+
+    def length_filtered_list(n)
+      merged_words_lists.select{ |word| word.length == n }
     end
 
-    def ordered_words_list
-      File.open(@words_count_path).read
+    def merged_words_lists
+      (most_used_words_sorted_list + words_list).uniq
+    end
+
+    def words_list
+      File.open(words_list_path).read.split("\n")
+    end
+
+    def most_used_words_sorted_list
+      File.open(most_used_words_list_path).read
         .split("\n")
         .map{ |str| str.match(/[a-z]{1,}/)[0] }
     end
@@ -36,15 +42,15 @@ class WordList
     def import_words_list
       uri = URI(WORDS_LIST_URL)
       response = Net::HTTP.get(uri)
-      File.open(@words_list_path,"w") do |f|
+      File.open(words_list_path,"w") do |f|
         f.write(response)
       end
     end
 
-    def import_words_count
-      uri = URI(WORDS_COUNT_URL)
+    def import_most_used_words_sorted_list
+      uri = URI(MOST_USED_WORDS_LIST_URL)
         response = Net::HTTP.get(uri)
-        File.open(@words_count_path,"w") do |f|
+        File.open(most_used_words_list_path,"w") do |f|
           f.write(response)
         end
     end
